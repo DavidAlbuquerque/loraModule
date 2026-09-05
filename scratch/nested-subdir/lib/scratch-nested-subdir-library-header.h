b6c271cfff952463037ab2776c7dfe9dca74851c
David <davidgabriel2102@alu.ufc.br>
2026-09-05 17:01:34 -0300

    update

--- scratch/nested-subdir/lib/scratch-nested-subdir-library-header.h
+++ scratch/nested-subdir/lib/scratch-nested-subdir-library-header.h
@@ -6,6 +6,9 @@
 // in a scratch subdirectory, one can create new programs that are implemented
 // in multiple files and headers.
 
+#ifndef SCRATCH_NESTED_SUBDIR_LIBRARY_HEADER_H
+#define SCRATCH_NESTED_SUBDIR_LIBRARY_HEADER_H
+
 #include <string>
 
 namespace ns3
@@ -14,8 +17,10 @@ namespace ns3
 /**
  * Get a message from the subdir.
  *
- * \return The message from the subdir
+ * @return The message from the subdir
  */
 std::string ScratchNestedSubdirGetMessage();
 
 } // namespace ns3
+
+#endif // SCRATCH_NESTED_SUBDIR_LIBRARY_HEADER_H
--- scratch/subdir/scratch-subdir-additional-header.h
+++ scratch/subdir/scratch-subdir-additional-header.h
@@ -6,6 +6,9 @@
 // in a scratch subdirectory, one can create new programs that are implemented
 // in multiple files and headers.
 
+#ifndef SCRATCH_SUBDIR_ADDITIONAL_HEADER_H
+#define SCRATCH_SUBDIR_ADDITIONAL_HEADER_H
+
 #include <string>
 
 namespace ns3
@@ -14,8 +17,10 @@ namespace ns3
 /**
  * Get a message from the subdir.
  *
- * \return The message from the subdir
+ * @return The message from the subdir
  */
 std::string ScratchSubdirGetMessage();
 
 } // namespace ns3
+
+#endif // SCRATCH_SUBDIR_ADDITIONAL_HEADER_H
